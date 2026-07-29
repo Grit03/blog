fcfbb4aa249be5f14f22a5c6df9567bf3dfda43f
GyuriKim <artgr2062@konkuk.ac.kr>
2026-07-29 21:49:55 +0900

    feat: 블로그 카테고리 추가

--- lib/categories.ts
+++ lib/categories.ts
@@ -3,6 +3,7 @@ export const CATEGORIES = [
   { slug: "deepdive", label: "딥다이브", value: "딥다이브" },
   { slug: "study", label: "학습정리", value: "학습정리" },
   { slug: "retro", label: "회고", value: "회고" },
+  { slug: "opensource", label: "오픈소스", value: "오픈소스"}
 ] as const;
 
 export type CategorySlug = (typeof CATEGORIES)[number]["slug"];
